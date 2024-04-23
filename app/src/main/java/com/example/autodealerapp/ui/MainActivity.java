4c2e9f00d25a9ffe1bc722afea74cabfb6a99b32
İsmail Can Varlı <icanvarli@outlook.com>
2024-04-24 00:44:50 +0300

    Floating action button added. Toolbar and item_car xml deleted.

--- app/src/main/java/com/example/autodealerapp/ui/MainActivity.java
+++ app/src/main/java/com/example/autodealerapp/ui/MainActivity.java
@@ -2,20 +2,35 @@ package com.example.autodealerapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.autodealerapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Araçları göstermek için RecyclerView nesnesi
+    private RecyclerView carRecyclerView;
+    //
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Fab butonu tanımla
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddCarActivity.class);
+            startActivity(intent);
+        });
+
+        /* todo: burayı kaldıracağım ya da düzelteceğim.
         // Ekleme, Gösterme, Güncelleme butonlarını
         Button addCarButton = findViewById(R.id.addCarButton);
         Button showCarButton = findViewById(R.id.showCarButton);
@@ -45,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("isItemClickable", true);
             startActivity(intent);
         });
+         */
+
+
+
+
 
     }
 }
